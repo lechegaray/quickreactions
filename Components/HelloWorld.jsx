e700259a52e41d38a2074beeaf7a11bd947a6e6f
Luis Echegaray <mrluisechegaray@gmail.com>
2015-11-23 09:19:33 -0500

    add first react component

---
+++ Components/HelloWorld.jsx
@@ -0,0 +1,12 @@
+var React = require('react')
+
+module.exports = React.createClass({
+  render: function() {
+    return(
+      <div>
+        This is from the HelloWorld.jsx
+        component render function.
+      </div>
+    )
+  }
+})
